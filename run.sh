75446c7b3d0f536d3a9f137cc02cacfe86ab1c8a
btemperli <beat@temper.li>
2025-04-20 11:42:17 +0200

    add run-script

---
+++ run.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo '------'
+echo 'ls -la'
+ls -la
+
+echo '------------'
+echo 'READY TO RUN'
+echo '------------'
+
+echo ''
+echo ' - start main - '
+echo ''
+sudo python3 main.py
