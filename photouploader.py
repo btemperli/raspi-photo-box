cc53675bc846d9efca2c08e5103d7e5616ff2043
btemperli <beat@temper.li>
2025-04-20 16:31:03 +0200

    prepare upload-function

--- photouploader.py
+++ photouploader.py
@@ -1,6 +1,6 @@
 import threading
 import time
-
+import requests
 import global_variables as glv
 
 
@@ -22,7 +22,7 @@ class PhotoUploader:
             print('output the message: ', self.message_output)
 
         glv.INSTANCE_DISPLAY.set_output_message(self.message_output)
-        time.sleep(20)
+        time.sleep(10)
         glv.INSTANCE_DISPLAY.set_output_message(None)
 
     def check_connection(self):
@@ -36,8 +36,26 @@ class PhotoUploader:
             self.thread_output = threading.Thread(target=self.output)
             self.thread_output.start()
 
-        image = glv.last_image
-        print("todo: upload photo (" + glv.last_image + ")")
+        image_path = glv.last_image
+        upload_url = glv.ENV_PHOTOBOX_URL_UPLOAD
+        token = glv.ENV_PHOTOBOX_TOKEN
+
+        try:
+            with open(image_path, 'rb') as image_file:
+                files = {'photo': image_file}
+                headers = {'token': token}
+                response = requests.post(upload_url, files=files, headers=headers)
+
+            if response.status_code == 200:
+                self.message_output = "Photo uploaded!"
+            else:
+                self.message_output = f"Upload failed, {response.status_code}: {response.text}"
+
+        except Exception as e:
+            self.message_output = f"Upload error: {str(e)}"
+
+        self.thread_output = threading.Thread(target=self.output)
+        self.thread_output.start()
 
     def shut_down(self):
         if glv.DEBUG:
